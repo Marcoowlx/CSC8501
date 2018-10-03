0aa1b82a85139e0b070e221435547b5638578f60
Marco Wang (Student) <b8011457@newcastle.ac.uk>
2018-10-03 13:45:37 +0100

    Pointer

--- Pointer.cpp
+++ Pointer.cpp
@@ -2,40 +2,30 @@
 
 using namespace std;
 
-int main() {
+void swaped(int c, int d) {
+	int *a = &c;
+	int *b = &d;
+	cout << "The value of the 1st nmumber is " << c << "\n";
+	cout << "The poiter value to the 1st nmumber is " << *a << "\n";
 
-	int a = 100;
+	cout << "The value of the 2nd nmumber is " << d << "\n";
+	cout << "The poiter value to the 2nd nmumber is " << *b << "\n";
+	int s = *a;
+	*a = *b;
+	*b = s;
+	cout << "The value of the 1st nmumber is " << c << "\n";
+	cout << "The poiter value to the 1st nmumber is " << *a << "\n";
 
-	cout << "Value in memory location a is " << a << "\n";
+	cout << "The value of the 2nd nmumber is " << d << "\n";
+	cout << "The poiter value to the 2nd nmumber is " << *b << "\n";
+}
 
-	cout << "Address of (the reference to) memory location a is " << &a << "\n";
-
-	int *b = &a;
-
-	cout << "Value in memory location pointed to by b is " << *b << "\n";
-
-
-	cout << "Value in memory location b is " << b << "\n";
-	cout << b << " should be the same as the memory location for a: " << &a << "\n";
-	cout << "b is actually at memory location " << &b << endl;
-
-	int** c;
-
-	c = &b;
-	cout << "c is now pointing to what b points to: " << **c << endl;
-
-	cout << "c points to: " << *c << endl;
-
-	cout << "c actually holds the value: " << c << endl;
-	cout << c << " is the same value as b's memory location: " << &b << endl;
-	cout << "c is actually at memory location " << &c << endl;
-
-	cout << "c is eventually retrieving what is in memory location " << **&c << endl;
-	cout << "the value of a is in memory location " << &a << endl;
-
-	int x;
-	cout << "Type in a numebr to finish" << "\n";
-	cin >> x;
-
-	return 0;
+void main() {
+	int a, b;
+	cout << "Input a number:" << "\n";
+	cin >> a;
+	cout << "Input another number:" << "\n";
+	cin >> b;
+	swaped(a, b);
+	system("pause");
 }
